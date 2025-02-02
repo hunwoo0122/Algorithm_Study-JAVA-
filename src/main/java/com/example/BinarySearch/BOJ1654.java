1ab4b9015f8dec96645040284cfc13bbdeb13352
ParkHeonWoo <phwoo1315@naver.com>
2025-02-02 22:23:42 +0900

    알고리즘 공부

---
+++ src/main/java/com/example/BinarySearch/BOJ1654.java
@@ -0,0 +1,55 @@
+package com.example.BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(),"");
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        max++;
+
+        long min = 0;
+        long mid = 0;
+
+        while (min < max) {
+
+            //범위 내에서 중간 길이를 구한다.
+            mid = (min + max)/2;
+
+            long count = 0;
+
+            // 구해진 중간 길이로 잘라서 총 몇 개가 만들어지는지를 구한다.
+
+            for (int i = 0; i < N; i++) {
+                count += (arr[i] / mid);
+            }
+
+            if(count < N) {
+                max = mid;
+            }
+            else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
